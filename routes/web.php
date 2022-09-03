8654b737953cc3ae5f407853a5b7df07de99eb02
saifltr <bagmarusaifs@gmail.com>
2022-09-03 10:05:12 +0530

    Routes and Controller Setup

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Tasks\TaskController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('app');
+});
+
+Route::get('/tasks', function () {
+    return view('tasks');
 });
